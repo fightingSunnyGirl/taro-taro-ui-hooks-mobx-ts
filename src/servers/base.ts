ba9762cfd9baf58568d782cabc8fd499b80fe5b6
myh <myh314126085@163.com>
2020-09-12 17:39:22 +0800

    优化数据管理

--- src/servers/base.ts
+++ src/servers/base.ts
@@ -1,6 +1,6 @@
 // 设置不同环境不同路径请求的url地址
 const NODE_ENV = process.env.NODE_ENV;
-const TARO_ENV = process.env.TARO_ENV;//开发环境区分 h5、小程序、支付宝等
+export const TARO_ENV = process.env.TARO_ENV;//开发环境区分 h5、小程序、支付宝等
 // 开发域名 h5开发环境使用代理
 const DEVDOMAIN = (TARO_ENV =='h5' && NODE_ENV == 'development') ? '/proxy': 'http://www.sunny.com';
 // 正式域名
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,52 @@
+import Taro from '@tarojs/taro';
 import counterStore from './counter'
+import { TARO_ENV } from '@servers/base';
 
 const stores:any = {
   counterStore
 }
 
+let storeStemps: any = Taro.getStorageSync("store") || null;
+
+// const detail = JSON.parse(Taro.getStorageSync('detail')) || null;
+// const hasUserInfo = Taro.getStorageSync('hasUserInfo') || false;
+
+//在页面加载时读取sessionStorage里的状态信息
+if (storeStemps) {
+  storeStemps = JSON.parse(storeStemps);
+  // 拷贝对应的值 而不是整个对象 保存时缺失了一些函数等深层次数据
+  for (let k in storeStemps) {
+    for (let i in storeStemps[k]) {
+      stores[k][i] = storeStemps[k][i]
+    }
+  }
+}
+
+//在页面刷新时将store里的信息保存到sessionStorage里 setStorage removeStorage setStorage
+if (TARO_ENV == 'h5') {
+  let u = navigator.userAgent;
+  if (u.indexOf("iPhone") > -1 || u.indexOf("iOS") > -1) {
+    window.addEventListener("pagehide", () => {
+      Taro.setStorage({
+        key:'store',
+        data:JSON.stringify(stores)
+      })
+    })
+  } else {
+    window.addEventListener("beforeunload", () => {
+      Taro.setStorage({
+        key:'store',
+        data:JSON.stringify(stores)
+      })
+    })
+  }
+} else {
+  Taro.onAppHide(()=>{
+    Taro.setStorage({
+      key:'store',
+      data:JSON.stringify(stores)
+    })
+  })
+}
 
 export default stores;
